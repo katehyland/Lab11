84e38936c1a4b94a0a96181b2a6221c7c17bcf9a
Kate Hyland <katehyland@quickenloans.com>
2019-11-18 21:53:46 -0500

    Initial Commit

---
+++ Lab11/Movie.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace Lab11
+{
+    public class Movie
+    {
+        private string title;
+        private string category;
+
+        //create a constructor that accepts a title & category as paramters
+
+
+        public string Title
+        {
+            get { return title;  }
+            set { title = value; }
+        }
+
+        public string Category
+        {
+            get { return category; }
+            set { category = value; }
+        }
+        public Movie(string title, string category)
+        {
+            Title = title;
+            Category = category;
+        }
+
+       
+    }
+}
---
+++ Lab11/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            bool repeat = true;
+            while (repeat)
+            {
+                List<Movie> movieList = new List<Movie>()
+            {
+
+                {
+                    new Movie("Step Brothers", "Drama")
+                },
+                {
+                    new Movie("Totoro", "Animated")
+                },
+                {
+                    new Movie("Lion King", "Drama")
+                },
+                {
+                    new Movie("IT", "Horror")
+                },
+                {
+                    new Movie("No Country For Old Men", "Horror")
+                },
+                {
+                    new Movie("Weekend At Bernies", "Drama")
+                },
+                {
+                    new Movie("King", "Drama")
+                },
+                {
+                    new Movie("Interstellar", "Sci Fi")
+                },
+                {
+                    new Movie("Inception", "Sci Fi")
+                },
+                {
+                    new Movie("Honey I Shrank The Kids", "Sci Fi")
+                },
+
+            };
+                Console.WriteLine("Enter a category: Animated, Drama, Horror, Sci Fi");
+                string input = Console.ReadLine();
+
+                foreach (Movie movie in movieList)
+                {
+                  
+                    if (input.ToUpper() != movie.Category)
+                    {
+                        Console.WriteLine("Input was not recognized as one of the four categories.");
+                        repeat = false;
+                    }
+                    if (input == movie.Category)
+                    {
+                        Console.WriteLine(movie.Title);
+                    }
+
+                  
+                };
+                Console.WriteLine("Would you like to continute? Enter yes or no.");
+                string continueInput = Console.ReadLine();
+
+                string contFormattedInput = continueInput.ToLower();
+
+                if (contFormattedInput == "no")
+                {
+                    Console.WriteLine("INPUT = NO");
+                    repeat = false;
+                }
+                if (contFormattedInput == "yes")
+                {
+                    Console.WriteLine("INPUT = NO");
+                    repeat = false;
+                }
+            }
+        }
+    }
+}
